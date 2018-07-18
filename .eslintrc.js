2a28ac008d50ed3d11265379101da0e98629e207
elvin <970099734@qq.com>
2018-07-18 17:41:36 +0800

    no message

--- .eslintrc.js
+++ .eslintrc.js
@@ -82,6 +82,7 @@ module.exports = {
     'function-paren-newline': [0],
     'no-restricted-globals': [0],
     'require-yield': [1],
+    'font-family-no-missing-generic-family-keyword': [0],
   },
   parserOptions: {
     ecmaFeatures: {
